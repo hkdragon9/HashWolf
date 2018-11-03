851a9896eaf94e1a0bd8ba60563c598a209ec071
Ken Guan <ruihan.guan@you.world>
2018-11-03 06:26:14 -0700

    fix minor error

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -10,6 +10,7 @@ container.setAttribute('class', 'container');
 app.appendChild(logo);
 app.appendChild(container);
 
+
 var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
