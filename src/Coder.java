6974a43ca9bf8f5f83f2339fe599b2d734349c3a
hkdragon9 <kennyhuang9@berkeley.edu>
2018-11-03 13:56:21 -0700

    Add Characteristics Map

--- src/Coder.java
+++ src/Coder.java
@@ -8,13 +8,28 @@ public class Coder extends Student {
     private String positions;
     private String commitment;
 
+    private int graduationInt;
+    private int majorInt;
+    private int skillsInt;
+    private int positionsInt;
+    private int commitmentInt;
 
     private Heap heap;
 
-    Coder(String n, String g, String m) {
+    Coder(String n, String g, String m, String s, String p, String c) {
         this.name = n;
         this.graduation = g;
         this.major = m;
+        this.skills = s;
+        this.positions = p;
+        this.commitment = c;
+
+        this.graduationInt = characteristics.get(g);
+        this.majorInt = characteristics.get(m);
+        this.skillsInt = characteristics.get(s);
+        this.positionsInt = characteristics.get(p);
+        this.commitmentInt = characteristics.get(c);
+
     }
 
 
@@ -27,8 +42,42 @@ public class Coder extends Student {
         return 0;
     }
 
-    public void setChar(String s, int n) {
 
+    //this method is for us to manually change int values of characteristics.
+    public void setChar(String charact, int n) {
+        if (charact.equals("graduation")) {
+            graduationInt = n;
+        } else if (charact.equals("major")) {
+            majorInt = n;
+        } else if (charact.equals("skills")) {
+            skillsInt = n;
+        } else if (charact.equals("positions")) {
+            positionsInt = n;
+        } else if (charact.equals("commitment")) {
+            commitmentInt = n;
+        }
+    }
+
+    //this method is for the user to change their information
+    public void setChar(String charact, String s) {
+        if (charact.equals("name")) {
+            name = s;
+        } else if (charact.equals("graduation")) {
+            graduation = s;
+            graduationInt = characteristics.get(s);
+        } else if (charact.equals("major")) {
+            major = s;
+            majorInt = characteristics.get(s);
+        } else if (charact.equals("skills")) {
+            skills = s;
+            skillsInt = characteristics.get(s);
+        } else if (charact.equals("positions")) {
+            positions = s;
+            positionsInt = characteristics.get(s);
+        } else if (charact.equals("commitment")) {
+            commitment = s;
+            commitmentInt = characteristics.get(s);
+        }
     }
 
     public void getChar(String s, int n) {
--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.*;
+
+
 public class Student {
 
 
@@ -8,14 +12,64 @@ public class Student {
     private String skills;
     private String positions;
     private String commitment;
+
+    private int graduationInt;
+    private int majorInt;
+    private int skillsInt;
+    private int positionsInt;
+    private int commitmentInt;
     //and more
 
-    private Heap heap;
+    //private Heap heap;
 
-    Coder(String n, String g, String m) {
-        this.name = n;
-        this.graduation = g;
-        this.major = m;
+
+    public static TreeMap<String, Integer> characteristics = new TreeMap<>();
+
+
+    
+
+
+
+
+
+    Student() {
+
+        characteristics.put("EECS", 10);
+        characteristics.put("Math", 7);
+        characteristics.put("Data Science", 9);
+        characteristics.put("Statistics", 8);
+        characteristics.put("Business", 3);
+        characteristics.put("Media Studies", 1);
+
+        characteristics.put("Freshman", 1);
+        characteristics.put("Sophomore", 2);
+        characteristics.put("Junior", 3);
+        characteristics.put("Senior", 4);
+        characteristics.put("Graduate", 5);
+        characteristics.put("PHD", 6);
+
+        characteristics.put("Java", 3);
+        characteristics.put("Python", 3);
+        characteristics.put("SQL", 2);
+        characteristics.put("Machine Learning", 5);
+        characteristics.put("Excel", 1);
+        characteristics.put("Microsoft office", 1);
+
+        characteristics.put("Backend Software Engineer", 5);
+        characteristics.put("Frontend Software Engineer", 10);
+        characteristics.put("IOS Software Engineer", 9);
+        characteristics.put("WebDev Software Engineer", 9);
+        characteristics.put("FullStack Software Engineer", 7);
+        characteristics.put("Data Analyst", 5);
+        characteristics.put("Project Manager", 1);
+
+        characteristics.put("4 Hours per Week", 1);
+        characteristics.put("8 Hours per Week", 2);
+        characteristics.put("12 Hours per Week", 3);
+        characteristics.put("16 Hours per Week", 4);
+        characteristics.put("20 Hours per Week", 5);
+
+
     }
 
 
@@ -32,7 +86,8 @@ public class Student {
 
     }
 
-    public void getChar(String s, int n) {
+    public int getChar(String s) {
+        return
 
     }
 
@@ -53,7 +108,7 @@ public class Student {
 
     }
 
-    
+
 
 
 
